22d36a0d105a8bf6ab4d8e9f11bafe8f412b6cea
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2025-05-15 04:18:45 +0530

    Vercel added:

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const todoList = [
+    {
+      title: "This is todo1",
+      description: "todo1",
+      id: 1,
+    },
+    {
+      title: "This is todo2",
+      description: "todo2",
+      id: 2,
+    },
+  ];
+
+  res.json(todoList);
+});
+
+app.listen(port, () => {
+  console.log("Server is running !");
+});
